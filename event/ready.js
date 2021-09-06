ecf296c30eda7ee974f06fad0f1352a8676f2714
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-06 23:53:32 +0600

    dynamic prefix in status

--- event/ready.js
+++ event/ready.js
@@ -1,6 +1,6 @@
 const client = require('../index')
 
 client.on('ready', () => {
-    client.user.setPresence({activities: [{name: "v!help", type: "WATCHING"}]})
+    client.user.setPresence({activities: [{name: `${client.prefix}help`, type: "WATCHING"}]})
     console.log(`🟢 ${client.user.tag} is online!`)
 })
