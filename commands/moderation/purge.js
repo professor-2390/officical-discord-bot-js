21f1b24753280e2d7b2095eaecb567651c094bc1
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-06 23:50:38 +0600

    fixed the 100 purge crashing bot error

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,34 +1,32 @@
-const {
-    Client,
-    Message,
-    MessageEmbed
-} = require('discord.js');
+const { Client, Message } = require("discord.js");
 
 module.exports = {
-    name: 'purge',
-    aliases: ['clear'],
-    UserPerms: ['MANAGE_MESSAGES'],
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
-     */
-    run: async (client, message, args, Discord) => {
-        try {
-            let delamount = args[0];
-            if (isNaN(delamount) || parseInt(delamount <= 0)) return message.reply('Error:')
+  name: "purge",
+  aliases: ["clear"],
+  UserPerms: ["MANAGE_MESSAGES"],
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args, Discord) => {
+    try {
+      let delamount = args[0];
+      if (isNaN(delamount) || parseInt(delamount <= 0))
+        return message.reply("Error:");
 
-            if (parseInt(delamount) > 100) return message.channel.send('you cant delete 100 messages at a time!')
+      if (parseInt(delamount) > 100 || parseInt(delamount) === 100)
+        return message.channel.send("you cant delete 100 messages at a time!");
 
-            await message.channel.bulkDelete(parseInt(delamount) + 1, true);
+      await message.channel.bulkDelete(parseInt(delamount) + 1, true);
 
-            await message.channel.send('Purged!').then(m => {
-                setTimeout(() => {
-                    m.delete()
-                }, 5000)
-            })
-        } catch (e) {
-            console.log(e)
-        }
+      await message.channel.send("Purged!").then((m) => {
+        setTimeout(() => {
+          m.delete();
+        }, 5000);
+      });
+    } catch (e) {
+      console.log(e);
     }
-}
+  },
+};
