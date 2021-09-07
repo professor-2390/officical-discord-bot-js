531bdd199914188281d95aa0231d48f88f142738
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-07 07:16:12 -0400

    hmmmmmmmmmmmmmmmmmmmm

---
+++ commands/fun/hmm.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "hmm",
+    aliases: [],
+    description: "hmmmmmmmmmmmmmmmm.",
+    UserPerms: ["SEND_MESSAGES"],
+    run: async (client, message, args, Discord) => {
+        message.channel.send("hmmmmmmmm :thinking:")
+    }
+}
