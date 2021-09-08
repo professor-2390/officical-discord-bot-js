562a906e9b4da5f487979e0b17796896267842fb
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-08 21:25:17 +0600

    😂 removed rmeme command and made it meme command

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,13 +1,39 @@
-const { Random } = require("something-random-on-discord")
-
+const { MessageEmbed } = require("discord.js");
+const redditFetch = require("reddit-fetch");
 
 module.exports = {
-    name: 'meme',
-    aliases: ['lol'],
-    description: 'Shows a random meme to make you laugh.',
-    UserPerms: ['SEND_MESSAGES'],
-    run: async (client, message, args, Discord) => {
-        let data = await Random.getMeme()
-        message.channel.send({embeds: [data.embed]})
-    }
-}
+  name: "meme",
+  aliases: [],
+  description: "Memes that will make you laught :joy:.",
+  UserPerms: ["SEND_MESSAGES"],
+
+  run: async (client, message, args, Discord) => {
+    redditFetch({
+      subreddit: "meme",
+      sort: "top",
+      allowNSFW: true,
+      allowModPost: false,
+      allowCrossPost: false,
+      allowVideo: false,
+    }).then((post) => {
+      const embed = new MessageEmbed()
+        .setTitle(`${post.title}`)
+        .setColor("DARK_PURPLE")
+        .setURL(post.url)
+        .setImage(post.url)
+        .addField("Subreddit:", post.subreddit_name_prefixed, true)
+        .addField("Author:", post.author, true)
+        .setFooter(
+          `Requested by ${message.author.username} 👍 ups: ${post.ups} 👎 downs: ${post.downs}`,
+          message.author.displayAvatarURL({
+            dynamic: true,
+            format: "png",
+            size: 2048,
+          })
+        )
+        .setTimestamp();
+        // console.table(post);
+      message.channel.send({ embeds: [embed] });
+    });
+  },
+};
