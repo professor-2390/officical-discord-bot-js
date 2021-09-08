221123f563a1fe0455b0afd4b5dd81987983cb9d
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-08 20:23:14 +0600

    🧊 updated slash command handler

--- handler/slash_commands.js
+++ handler/slash_commands.js
@@ -1,49 +1,41 @@
-const chalk = require('chalk');
-const fs = require('fs');
-const {
-    commands
-} = require('../index');
-const {
-    readdirSync
-} = fs;
-const client = require('../index');
+const chalk = require("chalk");
+const fs = require("fs");
+const { commands } = require("../index");
+const { readdirSync } = fs;
+const client = require("../index");
 
-console.log(chalk.blue.bold('Slash Commands'))
-readdirSync('./slashCommands').forEach(async (dir) => {
-    const commands = readdirSync(`./slashCommands/${dir}/`).filter((file) => 
-        file.endsWith(".js")
-    )
+console.log(chalk.blue.bold("Slash Commands"));
+readdirSync("./slashCommands").forEach(async (dir) => {
+  const commands = readdirSync(`./slashCommands/${dir}/`).filter((file) =>
+    file.endsWith(".js")
+  );
 
+  commands.map(async (cmd) => {
+    let file = require(`../slashCommands/${dir}/${cmd}`);
 
-        commands.map(async cmd => {
-            let file = require(`../slashCommands/${dir}/${cmd}`);
+    let name = file.name || "No command name.";
+    let description = file.description || "No Description";
+    let options = file.options || [];
 
-            let name = file.name || "No command name.";
-            let description = file.description || "No Description";
-            let options = file.options || [];
+    const data = {
+      name,
+      description,
+      options,
+    };
 
-            const data = {
-                name,
-                description,
-                options
-            }
+    let option = name == "No command name." ? "❌" : "✅";
 
-            let option = name == "No command name." ? '❌' : '✅';
+    console.log(`Loaded Slash Command ${option} | ${name}`);
 
-            console.log(`Loaded Slash Command ${option} | ${name}`);
+    if (option == "✅") {
+      setTimeout(async () => {
+        client.slash_commands.set(name, {
+          ...data,
+          run: file.run,
+        });
+        await client.guilds.cache.get('your guild id').commands.create(data)
+      }, 2500);
+    }
+  });
 
-            if (option == '✅') {
-                setTimeout(async () => {
-                    client.slash_commands.set(name, {
-                        ...data,
-                        run: file.run
-                    });
-                    // await client.guilds.cache.get('881887185228402771').commands.create(data) //let me get my guidl id
-
-                }, 2500);
-            
-            }
-        })        
-
-    //that should fix it
-})
+});
