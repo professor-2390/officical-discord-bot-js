7d5318f68c446a79110bb60feab2e4cbf21693f1
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-08 07:57:38 -0400

    ✔️ fixed non prefix command working bug

--- event/MessageCreate.js
+++ event/MessageCreate.js
@@ -1,17 +1,17 @@
 const client = require("../index");
-const { Discord, MessageEmbed } = require("discord.js");
 
 client.on("messageCreate", async (message) => {
-  if (message.author.bot || !message.guild) return;
-
+  if (message.author.bot || !message.guild) return; //To make your code more easier we will add discord to our parameters
+  if (!message.content.startsWith(client.prefix)) return;
   const [cmd, ...args] = message.content
-    .slice(client.config.PREFIX.length)
+    .slice(client.prefix.length)
     .trim()
     .split(" ");
-
+  const Discord = require("discord.js"); // add this
   let command = client.commands.get(cmd);
+  //make the const command to let command
 
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
+  if (!command) command = client.commands.get(client.aliases.get(cmd)); // I have already added it, then add
   if (command) {
     // User Perms
     if (!message.member.permissions.has(command.UserPerms || []))
@@ -25,15 +25,6 @@ client.on("messageCreate", async (message) => {
         `I need \`${command.BotPerms || []}\` Permissions`
       );
 
-    await command.run(client, message, args, Discord);
-  }
-  const Target = message.mentions.users.first();
-
-  if (Target === client.user) {
-    embed = new MessageEmbed()
-      .setTitle(`Hi, ${message.author.tag}`)
-      .setDescription(`My prefix is **${client.prefix}**`)
-      .setColor("DARK_PURPLE");
-    message.channel.send({ embeds: [embed] });
-  }
+    await command.run(client, message, args, Discord); // <= discord over there
+  } // This should be it lets test it out :D
 });
