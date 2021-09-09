78897711d7500c32edc06845099e97fc9fc88cd2
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-09 22:30:28 +0600

    😎 updated presence and added status

--- event/ready.js
+++ event/ready.js
@@ -1,6 +1,7 @@
 const client = require('../index')
 
 client.on('ready', () => {
-    client.user.setPresence({activities: [{name: `${client.prefix}help`, type: "WATCHING"}]})
+    client.user.setPresence({activities: [{name: `${client.guilds.cache.size} Servers | ${client.prefix}help`, type: "WATCHING"}]})
+    client.user.setStatus("dnd")
     console.log(`🟢 ${client.user.tag} is online!`)
 })
