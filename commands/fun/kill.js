b15d54958dee3b436678c57abbfc0b43f9708e75
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-11 05:23:35 -0400

    🔪 fixed bugs in kill command

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -12,20 +12,12 @@ module.exports = {
         try {
             const member = (await await message.mentions.members.first()) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((r) => r.user.username.toLowerCase().includes() === args.join(" ").toLocaleLowerCase()) || message.guild.members.cache.find((r) => r.displayName.toLowerCase().includes() === args.join(" ").toLocaleLowerCase());
             if (!member) {
-             return message.channel.send({
-              embed: {
-               color: 16734039,
-               description: `:knife: | You must mention someone to kill!\n\n**Usage:** \`${client.prefix} kill <user>\``,
-              },
-             });
+                const embed = new MessageEmbed().setDescription(`:knife: | You must mention someone to kill!\n\n**Usage:** \`${client.prefix}kill <user>\``).setColor("RED")
+                return message.channel.send({embeds: [embed]});
             }
-            if (message.author === member || message.member == member) {
-             return await message.channel.send({
-              embed: {
-               color: 16734039,
-               description: `:knife: | You can't kill yourself...`,
-              },
-             });
+            if (message.author === member || message.member === member) {
+                const embed = new MessageEmbed().setDescription(`:knife: | You can't kill yourself...`).setColor("RED")
+                return await message.channel.send({embeds: [embed]});
             }
             const pickeddeath = deaths[Math.floor(Math.random() * deaths.length)];
             const change1 = pickeddeath.replace("[NAME1]", "<@" + message.author + ">");
@@ -34,7 +26,7 @@ module.exports = {
              const response = await fetch("https://nekos.life/api/v2/img/slap");
              const body = await response.json();
              const embed = await new MessageEmbed() // Prettier
-              .setColor("RANDOM")
+              .setColor("RED")
               .setAuthor(
                "Tombstone of " + member.displayName + "!",
                message.author.displayAvatarURL({
@@ -59,7 +51,7 @@ module.exports = {
             message.channel.send({
              embed: {
               color: 16734039,
-              description: `Something went wrong... ${client.bot_emojis.sadness}`,
+              description: `Something went wrong... :sob:`,
              },
             });
            }
