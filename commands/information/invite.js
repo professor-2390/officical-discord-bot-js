0f3b69a6e8f506a117ccef46f873ac890a3303a3
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-11 13:44:19 +0600

    🎊 better invite command

--- commands/information/invite.js
+++ commands/information/invite.js
@@ -1,11 +1,16 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+
 module.exports = {
     name: "invite",
     aliases: [],
     description: "Want to add the bot add it now ⚜️.",
     UserPerms: ["SEND_MESSAGES"],
     run: async (client, message, args, Discord) => {
-        const embed = new MessageEmbed().setTitle("Add the bot now").setURL("https://discord.com/api/oauth2/authorize?client_id=881214410742448179&permissions=8&scope=bot%20applications.commands").setColor('RANDOM')
-        message.channel.send({embeds: [embed]})
+        const embed = new MessageEmbed().setTitle("Add VoidBot to your server").setDescription("Want to know how to add this wonderful bot to your server? nothing much just press the button.").setColor("DARK_PURPLE")
+        const row = new MessageActionRow().addComponents(
+            new MessageButton().setLabel("ADD THE BOT").setStyle("LINK").setURL("https://discord.com/api/oauth2/authorize?client_id=881214410742448179&permissions=8&scope=bot%20applications.commands")
+        )
+
+        message.reply({embeds: [embed], components: [row]})
     }
 }
