7eb39dcc12a021e20d96260c0000772854a6ce09
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-18 02:32:37 -0400

    ➖ remove role and embeded add role

--- commands/moderation/remove-role.js
+++ commands/moderation/remove-role.js
@@ -1,9 +1,9 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "add-role",
-  aliases: ["addrole"],
-  description: "Add a specific role to mentioned user.",
+  name: "remove-role",
+  aliases: ["removerole"],
+  description: "Remove a specific role from mentioned user.",
   UserPerms: ["MANAGE_ROLES"],
   /**
    * @param {Client} client
@@ -11,18 +11,16 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, message, args, Discord) => {
-       //lets use parameters (optional)
         /**
          * @param {Message} message
          */
-
         const target = message.mentions.members.first()
-        if(!target) return message.channel.send('No member specified')
+        if(!target) return message.channel.send('No member specified') 
         const role = message.mentions.roles.first()
         if(!role) return message.channel.send('No role specified')
 
-        await target.roles.add(role)
-        const embed = new MessageEmbed().setTitle("Role added").addField("To", `${target.user.username}`,true).addField("Role", `${role}`, true)
+        await target.roles.remove(role)
+        const embed = new MessageEmbed().setTitle("Role removed").addField("From", `${target.user.username}`,true).addField("Role", `${role}`, true)
         message.channel.send({embeds: [embed]})
   }, 
 };
