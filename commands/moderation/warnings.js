b446e4df87b69d6fb2aa4bb1d6c8fb6672278f8a
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-18 14:03:21 +0600

    👮‍♂️ made the warnings command better

--- commands/moderation/warnings.js
+++ commands/moderation/warnings.js
@@ -1,42 +1,45 @@
-const {
-    Client,
-    Message,
-    MessageEmbed
-} = require('discord.js');
-const warndb = require('../../models/warndb')
+const { Client, Message, MessageEmbed } = require("discord.js");
+const warndb = require("../../models/warndb");
 
 module.exports = {
-    name: 'warnings',
-    UserPerms: ["MANAGE_MESSAGES"],
-    aliases: ["warns"],
-    description: "View all warns of the mentioned user.",
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
-     */
-    run: async (client, message, args, Discord) => {
+  name: "warnings",
+  UserPerms: ["SEND_MESSAGES"],
+  aliases: ["warns"],
+  description: "View all warns of the mentioned user.",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args, Discord) => {
+    const user = message.mentions.members.first() || message.author.id;
 
-        const user = message.mentions.members.first() || message.author.id;
-
-        warndb.findOne({
-            guild: message.guild.id, 
-            user: user.id
-        }, async (err, data) => {
-            if (err) throw err
-            if (data) {
-                const e = data.content.map(
-                    (w, i) => `\n\`${i + 1}\` - Moderator: ${message.guild.members.cache.get(w.moderator).user.tag}, Reason: ${w.reason}`
-                )
-                const embed = new MessageEmbed()
-                    .setDescription(e.join(' '))
-                message.channel.send({
-                    embeds: [embed]
-                })
-            } else {
-                message.channel.send('This user does not have any warnings')
-            }
-        })
-
-    }
-}
+    warndb.findOne(
+      {
+        guild: message.guild.id,
+        user: user.id,
+      },
+      async (err, data) => {
+        if (err) throw err;
+        if (data) {
+          const e = data.content.map(
+            (w, i) =>
+              `\n\`${i + 1}\` - Moderator: ${
+                message.guild.members.cache.get(w.moderator).user.tag
+              }, Reason: ${w.reason}`
+          );
+          const embed = new MessageEmbed().setDescription(e.join(" "));
+          message.channel.send({
+            embeds: [embed],
+          });
+        } else {
+          if (user === message.author.id) {
+            message.channel.send("You do not have any warnings");
+          } else {
+            message.channel.send("This user does not have any warnings");
+          }
+        }
+      }
+    );
+  },
+};
