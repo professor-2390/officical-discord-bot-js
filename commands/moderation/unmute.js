33fd2e1086a5ee543e2c8ccdbfca93aaa24c0429
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-18 21:29:35 +0600

    😎 updated unmute command

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -4,20 +4,21 @@ module.exports = {
   name: "unmute",
   aliases: ["um"],
   description: "Unmutes the mentioned user.",
-  usage: "",
+  UserPerms: ["MANAGE_MESSAGES"],
   /**
    * @param {Client} client
    * @param {Message} message
    * @param {String[]} args
    */
   run: async (client, message, args, Discord) => {
-    const member = message.mentions.members.first();
-    let target = message.guild.members.cache.get(member.id);
-    const role = message.guild.roles.cache.find(
-      (role) => role.name === "Muted"
-    );
+    const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 
-    target.roles.remove(role.id);
-    message.reply("Removed Role!");
-  }, //lets try
+    if(!Member) return message.channel.send('Member not found')
+
+    const role = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted');
+
+    await Member.roles.remove(role)
+
+    message.channel.send(`${Member.displayName} is now unmuted`)
+  }, 
 };
