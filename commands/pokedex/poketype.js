2adc536e419d1eae1080093d9c9b05100a4f0108
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-20 06:59:38 -0400

    🔥 poketype command

---
+++ commands/pokedex/poketype.js
@@ -0,0 +1,54 @@
+const { MessageEmbed, MessageButton } = require("discord.js");
+const paginationEmbed = require("../../functions/paginatorembed");
+const Pokemon = require("pokemon.js");
+Pokemon.setLanguage("english");
+module.exports = {
+  name: "poketype",
+  aliases: [],
+  description: "what type is that pokemon.",
+  UserPerms: ["SEND_MESSAGES"],
+
+  run: async (client, message, args, Discord) => {
+    const text = args;
+    if (!text) {
+      message.channel.send({
+        content: "Tell a pokemon you want to know about...",
+      });
+    } else {
+
+      Pokemon.getPokemon(`${text}`).then(async (response) => {
+        if (!response) return message.channel.send("No pokemon found");
+  
+        const embed1 = new MessageEmbed()
+          .setTitle(`${response.name}`)
+          .setDescription(`Type: ${response.types[0].name}`)
+          .setThumbnail(response.sprites.front_shiny)
+  
+        const embed2 = new MessageEmbed()
+          .setTitle(`${response.name}`)
+          .setImage(response.sprites.front_shiny);
+  
+        const button1 = new MessageButton()
+          .setCustomId("previousbtn")
+          .setLabel("◀")
+          .setStyle("SUCCESS");
+  
+        const button2 = new MessageButton()
+          .setCustomId("nextbtn")
+          .setLabel("▶")
+          .setStyle("SUCCESS");
+  
+        // Create an array of embeds
+        pages = [embed1, embed2];
+  
+        //create an array of buttons
+  
+        buttonList = [button1, button2];
+  
+        paginationEmbed(message, pages, buttonList);
+        // console.log(response);
+      });
+    }
+
+  },
+};
