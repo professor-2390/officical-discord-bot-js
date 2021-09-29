e9645812a5ddd304ecdb4bab7c4175afd1003581
Sadman-Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2021-09-29 17:19:26 +0600

    ⬆ better uptime command

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -1,20 +1,31 @@
-const { Client, Message, MessageEmbed } = require('discord.js')
+const { Client, Message, MessageEmbed } = require("discord.js");
+const moment = require("moment");
 
 module.exports = {
-    name: 'uptime',
-    aliases: [],
-    UserPerms: ["SEND_MESSAGES"],
-    description: "Uptime of da bot.",
-    /**
-     * @param {Client} client
-     * @param {Message} message
-     * @param {String[]} args
-    */
-    run: async (client, message, args) => {
-        const days = Math.floor(client.uptime / 86400000);
-        const hours = Math.floor(client.uptime / 3600000) % 24;
-        const minutes = Math.floor(client.uptime / 60000) % 60;
-        const seconds = Math.floor(client.uptime / 1000) % 60;
-        message.channel.send({ content: `**__Uptime:__**\nWeky is online since ${days}**d **${hours}**h **${minutes}**m **${seconds}**s**!` });
-    }
-}
+  name: "uptime",
+  aliases: [],
+  UserPerms: ["SEND_MESSAGES"],
+  description: "Uptime of da bot.",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const days = Math.floor(client.uptime / 86400000);
+    const hours = Math.floor(client.uptime / 3600000) % 24;
+    const minutes = Math.floor(client.uptime / 60000) % 60;
+    const seconds = Math.floor(client.uptime / 1000) % 60;
+    const date = moment().format("dddd, MMMM Do YYYY");
+    const embed = new MessageEmbed()
+      .setTitle(`${client.user.username}'s **__Uptime:__**`)
+      .setDescription(
+        "```" + `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds` + "```"
+      )
+      .addField("Date Launched", date, false)
+      .setThumbnail(client.user.avatarURL({ dynamic: true }))
+      .setColor("DARK_PURPLE")
+      .setTimestamp()
+    message.channel.send({ embeds: [embed] });
+  },
+};
