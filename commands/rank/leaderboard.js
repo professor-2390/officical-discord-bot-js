0e21f351927ea54c4cc581aea9036fb8fce22248
Professor 2390 <61703195+professor-2390@users.noreply.github.com>
2021-10-06 13:13:49 +0600

    📊 better leaderboard command

--- commands/rank/leaderboard.js
+++ commands/rank/leaderboard.js
@@ -4,12 +4,12 @@ Levels.setURL(config.mongooseConnectionString);
 
 module.exports = {
   name: "leaderboard",
-  aliases: [],
+  aliases: ["lb"],
   description: "Get rank leaderboard data.",
   UserPerms: ["SEND_MESSAGES"],
 
   run: async (client, message, args, Discord) => {
-    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
+    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10);
 
     if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
@@ -17,17 +17,18 @@ module.exports = {
       client,
       rawLeaderboard,
       true
-    ); // We process the leaderboard.
+    );
 
     const lb = leaderboard.map(
       (e) =>
         `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${
-          e.level
+          e.level + 1
         }\nXP: ${e.xp.toLocaleString()}`
-    ); // We map the outputs.
+    );
 
     const embed = new Discord.MessageEmbed()
-      .setTitle("Leaderboard:")
+      .setTitle(`${message.guild.name}'s Leaderboard:`)
+      .setThumbnail(message.guild.iconURL({ dynamic: true }))
       .setDescription(`\n\n${lb.join("\n\n")}`)
       .setColor("DARK_PURPLE");
 
