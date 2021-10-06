32fc7c37f1c925b249f6f6f5616cecb846fe37b2
Professor 2390 <61703195+professor-2390@users.noreply.github.com>
2021-10-06 13:12:52 +0600

    😊 better user rank ember command

--- commands/rank/rank.js
+++ commands/rank/rank.js
@@ -9,18 +9,19 @@ module.exports = {
   UserPerms: ["SEND_MESSAGES"],
 
   run: async (client, message, args, Discord) => {
-    const target = message.mentions.users.first() || message.author; // Grab the target.
+    const target = message.mentions.users.first() || message.author;
 
-    const user = await Levels.fetch(target.id, message.guild.id); // Selects the target from the database.
+    const user = await Levels.fetch(target.id, message.guild.id);
 
     if (!user)
       return message.channel.send(
         "Seems like this user has not earned any xp so far."
-      ); // If there isnt such user in the database, we send a message in general.
+      ); 
 
     const embed = new Discord.MessageEmbed()
       .setTitle(`${target.username}'s Rank Embed`)
-      .addField("Current Level:", `${user.level}`, false)
+      .addField("Current Level:", `${user.level + 1}`, false)
+      .addField("Xp:", `${user.xp}`, false)
       .setThumbnail(target.avatarURL({dynamic: true}))
       .setColor("DARK_PURPLE")
       .setFooter(
@@ -33,6 +34,11 @@ module.exports = {
       )
       .setTimestamp();
 
-    message.channel.send({ embeds: [embed] }); // We show the level.
+    message.channel.send({ embeds: [embed] });
   },
 };
+
+/**
+ * Color the embed with user accent color
+ * add avatar art on it like level in avatar
+ */
